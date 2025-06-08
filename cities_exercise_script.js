4641be1c357f3ec1a6ea078de64a4af8d407b097
alec-somers <alecv1528@gmail.com>
2025-06-08 16:22:08 -0700

    events review

---
+++ cities_exercise_script.js
@@ -0,0 +1,9 @@
+// window.addEventListener("click", function () {
+//   alert("You clicked on the page!");
+// });
+
+var button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  alert("Someone clicked the button!");
+});
